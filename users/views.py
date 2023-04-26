6388397b38fd167f31816fa31989f35e8e537fe0
badgersky <szymonek.ptasznik@gmail.com>
2023-04-26 17:44:46 +0200

    create login view, html, and form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.views import View
+from . import forms
 
-# Create your views here.
+
+class LoginView(View):
+
+    def get(self, request):
+        form = forms.LoginForm()
+        return render(request, 'users/login.html', {'form': form})
+
+    def post(self, request):
+        form = forms.LoginForm(request, request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect(reverse('home:home'))
+
+        return render(request, 'users/login.html', {'form': form})
